05358b1513c7fe6a76cccef2c61f3750613340c7
kalebdias.oficial@gmai.com <kalebdias.oficial@gmail.com>
2021-09-10 22:46:30 -0400

    feat: implemented seletect and show fragment

--- app/src/main/java/br/com/gascheck/ui/MainActivity.kt
+++ app/src/main/java/br/com/gascheck/ui/MainActivity.kt
@@ -2,11 +2,19 @@ package br.com.gascheck.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import br.com.gascheck.R
+import br.com.gascheck.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        binding.bottomNavigation.setupWithNavController(navHostFragment.navController)
     }
 }
