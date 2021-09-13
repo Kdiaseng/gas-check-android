87459e5497719cbd9bf065928f3c256598a229c4
Kaleb Dias da Silva <kaleb.silva@ITB001106.itbam.org.br>
2021-09-13 15:17:30 -0400

    feat: create adpter to hisctoric list

---
+++ app/src/main/java/br/com/gascheck/ui/history/adapter/HistoricAdapter.kt
@@ -0,0 +1,57 @@
+package br.com.gascheck.ui.history.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import br.com.gascheck.R
+import br.com.gascheck.databinding.ItemDataGasBinding
+import br.com.gascheck.ui.model.GasDataUi
+
+class HistoricAdapter : ListAdapter<GasDataUi, HistoricAdapter.HistoricHolder>(object :
+
+    DiffUtil.ItemCallback<GasDataUi>() {
+    override fun areItemsTheSame(oldItem: GasDataUi, newItem: GasDataUi): Boolean =
+        oldItem.id == newItem.id
+
+    override fun areContentsTheSame(oldItem: GasDataUi, newItem: GasDataUi): Boolean =
+        oldItem.id == newItem.id
+
+}) {
+
+    inner class HistoricHolder(private val itemDataGasBinding: ItemDataGasBinding) :
+        RecyclerView.ViewHolder(itemDataGasBinding.root) {
+
+        fun bind(dataGas: GasDataUi) {
+
+            itemDataGasBinding.apply {
+                val context = this.root.context
+                textItemNameGasStation.text = dataGas.name
+                textItemTypeGas.text = dataGas.typeGas
+                textItemDateRegister.text = context.getString(
+                    R.string.item_value, dataGas.day, dataGas.month, dataGas.time
+                )
+            }
+
+
+        }
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): HistoricAdapter.HistoricHolder =
+        HistoricHolder(
+            ItemDataGasBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    
+
+    override fun onBindViewHolder(holder: HistoricAdapter.HistoricHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
---
+++ app/src/main/java/br/com/gascheck/ui/model/GasDataUi.kt
@@ -0,0 +1,13 @@
+package br.com.gascheck.ui.model
+
+data class GasDataUi(
+    val id: Int,
+    val name: String = "Desconhecido",
+    val typeGas: String,
+    val day: String,
+    val month: String,
+    val time: String,
+    var like: Boolean? = null,
+    var dislike: Boolean? = null,
+    val value: String
+)
